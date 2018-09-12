4091e2b03cbe4f05df337348ee0c16b322b599a7
Tzuchin Chang <soechin@gmail.com>
2018-09-12 14:23:28 +0800

    20180912_1423

--- ShiftRMB/MainWindow.xaml.cs
+++ ShiftRMB/MainWindow.xaml.cs
@@ -20,9 +20,61 @@ namespace ShiftRMB
     /// </summary>
     public partial class MainWindow : Window
     {
+        Winmm.TIMECALLBACK m_func;
+        int m_timer;
+        bool m_pressed;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            m_func = new Winmm.TIMECALLBACK(Timer_Elapsed);
+            m_timer = Winmm.timeSetEvent(100, 1, m_func, IntPtr.Zero, 1/*TIME_PERIODIC*/);
+            m_pressed = false;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Winmm.timeKillEvent(m_timer);
+        }
+
+        private void Timer_Elapsed(int uTimerID, int uMsg, IntPtr dwUser, IntPtr dw1, IntPtr dw2)
+        {
+            short ks = User32.GetAsyncKeyState(0xA5/*VK_RMENU*/);
+
+            if ((ks & 0x0001) != 0)
+            {
+                m_pressed = true;
+
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    MyTextBlock.Text = "Key Down";
+                }));
+            }
+            else if ((ks & 0x8000) == 0 && m_pressed)
+            {
+                m_pressed = false;
+
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    MyTextBlock.Text = "Key Up";
+                }));
+
+                User32.SendInput(new User32.KEYBDINPUT
+                {
+                    wVk = 0x10/*VK_SHIFT*/,
+                    wScan = (short)User32.MapVirtualKey(0x10/*VK_SHIFT*/, 0/*MAPVK_VK_TO_VSC*/),
+                });
+
+                User32.SendInput(new User32.KEYBDINPUT
+                {
+                    wVk = 0x02/*VK_RBUTTON*/,
+                    wScan = (short)User32.MapVirtualKey(0x02/*VK_RBUTTON*/, 0/*MAPVK_VK_TO_VSC*/),
+                });
+            }
         }
     }
 }
---
+++ ShiftRMB/Winmm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ShiftRMB
+{
+    public class Winmm
+    {
+        public delegate void TIMECALLBACK(int uTimerID, int uMsg, IntPtr dwUser, IntPtr dw1, IntPtr dw2);
+
+        [DllImport("winmm.dll")]
+        public static extern int timeSetEvent(int uDelay, int uResolution, TIMECALLBACK lpTimeProc, IntPtr dwUser, int fuEvent);
+
+        [DllImport("winmm.dll")]
+        public static extern int timeKillEvent(int uTimerID);
+    }
+}
